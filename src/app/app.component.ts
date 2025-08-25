90233deae50cc5347dfc8afed297e3c43ef48683
Alina <AliKochneva@yandex.ru>
2025-08-25 16:41:19 +0700

    order structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "./components/main/header/header.component";
 import { HeroComponent } from "./components/sections/hero/hero.component";
 import { MenuComponent } from "./components/sections/menu/menu.component";
+import { OrderComponent } from "./components/sections/order/order.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, HeroComponent, MenuComponent],
+  imports: [HeaderComponent, HeroComponent, MenuComponent, OrderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
