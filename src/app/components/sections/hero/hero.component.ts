db3bda61dae8f72b37af5ac7b13bb9acce88141d
Alina <AliKochneva@yandex.ru>
2025-08-25 15:30:06 +0700

    Разработана первый экран сайта

--- src/app/components/sections/hero/hero.component.ts
+++ src/app/components/sections/hero/hero.component.ts
@@ -1,12 +1,48 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
+export interface HeroFeature {
+  icon: string;
+  alt: string;
+  title: string;
+  text: string;
+}
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css'
 })
+
 export class HeroComponent {
+
+  features: HeroFeature[] = [
+    {
+      icon: '/images/icons/hop.svg',
+      alt: 'Лучшее тесто',
+      title: 'Лучшее тесто',
+      text: 'Мы создаем тесто только из отборной итальянской муки наивысшего качества',
+    },
+    {
+      icon: '/images/icons/kitchen-pack.svg',
+      alt: 'Лучшие повара',
+      title: 'Лучшие повара',
+      text: 'Пиццы готовят самые профессиональные итальянские повара',
+    },
+    {
+      icon: '/images/icons/seo-and-web.svg',
+      alt: 'Гарантия качества',
+      title: 'Гарантия качества',
+      text: 'Наша пиццерия получила множество наград и признаний по всему миру',
+    },
+    {
+      icon: '/images/icons/holidays.svg',
+      alt: 'Отборные рецепты',
+      title: 'Отборные рецепты',
+      text: 'Мы используем рецепты от мировых лидеров в изготовлении пиццы',
+    }
+  ];
 
 }
