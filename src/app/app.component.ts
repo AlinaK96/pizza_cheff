e71ba6a856d43075fdacd9e39adb3fb697459921
Alina <AliKochneva@yandex.ru>
2025-08-25 15:01:03 +0700

    Подключение шрифта Muller Regular/Extra Bold

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./components/main/header/header.component";
+import { HeroComponent } from "./components/sections/hero/hero.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, HeroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
